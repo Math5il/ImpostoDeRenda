2ca2c04650016bfe027c083aff476d40078d4c23
Matheus Santos <78796479+Math5il@users.noreply.github.com>
2025-05-19 16:51:27 -0300

    Criando Testes e alterando o pom xml
    Criando testes unitarios para o calculo de Imposto, e melhorando o xml para o teste.

--- src/test/java/com/matheus/AppTest.java
+++ src/test/java/com/matheus/AppTest.java
@@ -1,20 +1,57 @@
 package com.matheus;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    
+     @Test
+    public void testFaixaIsento() {
+        double renda = 27870.40;
+        double imposto = App.CalcularImposto(renda);
+        assertEquals(0.0, imposto, 0.01);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testFaixa075() {
+        double renda = 30000.00;
+        double imposto = App.CalcularImposto(renda);
+        // (30000 - 27870.40) * 0.075
+        double esperado = (30000.00 - 27870.40) * 0.075;
+        assertEquals(esperado, imposto, 0.01);
+    }
+
+    @Test
+    public void testFaixa15() {
+        double renda = 40000.00;
+        double imposto = App.CalcularImposto(renda);
+        double esperado = (40000.00 - 37751.55) * 0.15 +
+                          (37751.55 - 27870.40) * 0.075;
+        assertEquals(esperado, imposto, 0.01);
+    }
+
+    @Test
+    public void testFaixa225() {
+        double renda = 52000.00;
+        double imposto = App.CalcularImposto(renda);
+        double esperado = (52000.00 - 50148.25) * 0.225 +
+                          (50148.25 - 37751.55) * 0.15 +
+                          (37751.55 - 27870.40) * 0.075;
+        assertEquals(esperado, imposto, 0.01);
+    }
+
+    @Test
+    public void testFaixa275() {
+        double renda = 70000.00;
+        double imposto = App.CalcularImposto(renda);
+        double esperado = (70000.00 - 64259.59) * 0.275 +
+                          (64259.59 - 50148.25) * 0.225 +
+                          (50148.25 - 37751.55) * 0.15 +
+                          (37751.55 - 27870.40) * 0.075;
+        assertEquals(esperado, imposto, 0.01);
     }
 }
